63ea5f6d636ac9bfe5999feffb392cf01fe31df7
truongthicamhuong01-cmyk <truongthicamhuong01@gmail.com>
2026-04-18 20:31:32 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
